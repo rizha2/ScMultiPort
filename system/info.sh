a4fff835025d6b1816480d79de2ec95a800eb7a8
naszur27 <85011815+rizha2@users.noreply.github.com>
2023-01-23 14:49:21 +0800

    Add files via upload

---
+++ system/info.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#wget https://github.com/${GitUser}/
+GitUser="rizha2"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+clear
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+clear
+echo ""
+figlet " SCRIPT" | lolcat
+figlet " BY drgvpn" | lolcat
+echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
+echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
+echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
+echo -e "           \e[031;1m> Premium Script By NaszurTech <\e[0m" | lolcat
+echo -e " \e[0;32m_______________________________________________\e[0m"
+echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
+echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare NaszurTech         \e[0;32m|\e[0m"
+echo -e "\e[0;32m|               \e[1;35mI HOPE YOU HAPPY                \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________\e[36;1mTHANKYOU SUPPORT\e[0m\e[0;32m________________|\e[0m"
+echo -e "               \e[0;32m'----------------'\e[0m"
